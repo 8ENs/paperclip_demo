a8eec7bd76327b0e2a10b088af3a641290004dc4
8ENs <ben@coderush.ca>
2017-09-17 08:54:20 -0700

    ...

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,7 +1,8 @@
 class Friend < ActiveRecord::Base
   validates :name, presence: true
   validates :avatar,
-    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    # attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    attachment_content_type: { content_type: ['image/jpeg', 'application/pdf'] },
     attachment_size: { less_than: 4.megabytes }
 
   has_attached_file :avatar, styles: {
